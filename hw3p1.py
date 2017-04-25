e11431cb5531c4dbcbfab2ca79c3ecea26daa8f3
beerys <sara.beery@gmail.com>
2017-04-24 17:00:26 -0700

    new edits

--- hw3p1.py
+++ hw3p1.py
@@ -123,10 +123,10 @@ datagen = ImageDataGenerator()
 
 #fit the model (should I specify classes?  How do I split the training and test data)
 history = model.fit_generator(datagen.flow_from_directory(directory=train_folder, target_size=(256,256),classes=classes),
-					validation_data=datagen.flow_from_directory(directory=test_folder, batch_size=len(x_test_names), target_size=(256,256),classes=classes),
-					validation_steps=1,
+					validation_data=datagen.flow_from_directory(directory=test_folder, target_size=(256,256),classes=classes),
+					validation_steps=1000,
                     epochs=epochs,
-                    steps_per_epoch=len(x_train_names),
+                    steps_per_epoch=1000,
                     callbacks=callbacks,
                     verbose=1)
 
