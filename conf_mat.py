ae51b357d8b61cbd572953f59fe504a9911a5354
beerys <sara.beery@gmail.com>
2017-04-25 13:28:19 -0700

    edits

--- conf_mat.py
+++ conf_mat.py
@@ -74,7 +74,7 @@ else:
 image_folder = 'CUB_200_2011/CUB_200_2011/images'
 test_folder = 'Test'
 train_folder = 'Train'
-filepath = "Bird_Model_1.h5"
+filepath = "Bird_Model_2.h5"
 
 x_train_names, x_test_names, y_train, y_test, classes = get_data_info(num_ims)
 
